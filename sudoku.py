db241ef40235c3f984ebdc8f92e3603805da1b66
= <=>
2019-01-06 18:41:13 -0200

    Set boarg as global variable

--- sudoku.py
+++ sudoku.py
@@ -16,49 +16,45 @@ from collections import deque
 from math import sqrt
 import time
 
+_board = []
+
 def initialize(n, values):
     possible = deque(range(1, n+1))
     possible_str = [str(p) for p in possible]
-    board = []
     
     try:
         for i in range(n):
-            board.append([])
+            _board.append([])
             for j in range(n):
                 v = values[i*n + j]
                 if v in possible_str:
-                    board[i].append(deque([int(v)]))
+                    _board[i].append(deque([int(v)]))
                 else:
-                    board[i].append(possible.copy())
+                    _board[i].append(possible.copy())
     except:
-        draw(board)
+        draw()
         raise
-    return board
 
-def remove_dups_row(n, board, pos_i, pos_j):
-    value = board[pos_i][pos_j][0]
+def remove_dups_row(n, pos_i, pos_j):
+    value = _board[pos_i][pos_j][0]
 
     for i in range(n):
         if i == pos_i:
             continue
-        if value in board[i][pos_j]:
-            board[i][pos_j].remove(value)
+        if value in _board[i][pos_j]:
+            _board[i][pos_j].remove(value)
 
-    return board
-
-def remove_dups_col(n, board, pos_i, pos_j):
-    value = board[pos_i][pos_j][0]
+def remove_dups_col(n, pos_i, pos_j):
+    value = _board[pos_i][pos_j][0]
 
     for j in range(n):
         if j == pos_j:
             continue
-        if value in board[pos_i][j]:
-            board[pos_i][j].remove(value)
-    
-    return board
+        if value in _board[pos_i][j]:
+            _board[pos_i][j].remove(value)
 
-def remove_dups_quadrant(n, board, pos_i, pos_j):
-    value = board[pos_i][pos_j][0]
+def remove_dups_quadrant(n, pos_i, pos_j):
+    value = _board[pos_i][pos_j][0]
 
     quad_size = int(sqrt(n))
     quad_start_i = int(pos_i / quad_size) * quad_size
@@ -70,23 +66,21 @@ def remove_dups_quadrant(n, board, pos_i, pos_j):
         for j in range(quad_start_j, quad_end_j):
             if i == pos_i and j == pos_j:
                 continue
-            if value in board[i][j]:
-                board[i][j].remove(value)
-    
-    return board
+            if value in _board[i][j]:
+                _board[i][j].remove(value)
 
-def count(board):
+def count():
     total = 0
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            total += len(board[i][j])
+    for i in range(len(_board)):
+        for j in range(len(_board[i])):
+            total += len(_board[i][j])
     return total
 
-def draw(board):
-    for i in range(len(board)):
+def draw():
+    for i in range(len(_board)):
         print()
-        for j in range(len(board[i])):
-            for k in board[i][j]:
+        for j in range(len(_board[i])):
+            for k in _board[i][j]:
                 print(k, end='')
             print(end=' ')
     print()
@@ -101,8 +95,8 @@ def run(n, values):
     # Split values by space and new line into a list
     list_values = re.split(' |\n', values)
     
-    # Create a board after a list of values with all possible values filled in empty possitions
-    board = initialize(n, list_values)
+    # Create a _board after a list of values with all possible values filled in empty possitions
+    initialize(n, list_values)
 
     max_values = n**2
     last_count = 0
@@ -110,12 +104,12 @@ def run(n, values):
     while True:
         for i in range(n):
             for j in range(n):
-                if len(board[i][j]) == 1:
-                    board = remove_dups_row(n, board, i, j)
-                    board = remove_dups_col(n, board, i, j)
-                    board = remove_dups_quadrant(n, board, i, j)
+                if len(_board[i][j]) == 1:
+                    remove_dups_row(n, i, j)
+                    remove_dups_col(n, i, j)
+                    remove_dups_quadrant(n, i, j)
        
-        curr_count = count(board)
+        curr_count = count()
         
         if curr_count == max_values:
             break
@@ -130,8 +124,6 @@ def run(n, values):
     
     print('Elapsed time: {} milliseconds'.format(elapsed.microseconds / 1000))
     
-    draw(board)
-
-    return board
+    draw()
 
 run(9, values)
